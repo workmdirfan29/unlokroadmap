21fd5bbd121df4ca1ba9aa705b3a0fcbf4ac77be
xshshahab <mdshahbuddin9504@gmail.com>
2025-02-26 23:31:27 +0530

    Logo will be updating soon

--- src/components/Header/Title.jsx
+++ src/components/Header/Title.jsx
@@ -4,9 +4,15 @@ import Logo from "../../assets/Logo.png";
 const Title = ({ onClick }) => {
   return (
     <div onClick={onClick} className="flex items-center gap-2 cursor-pointer">
-      <img src={Logo} alt="logo" width={32} />
+      <img
+        src={Logo}
+        className="rounded-lg"
+        alt="logo"
+        width={42}
+
+      />
       <h1 className="hidden text-2xl font-bold md:block">
-        <span className="text-indigo-600">Roadmap</span>
+        <span className="text-purple-600">Roadmap</span>
       </h1>
     </div>
   );
